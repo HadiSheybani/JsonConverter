775902e1f4e1061bf95ad3697478183b12e6460f
hadisheybani <hadi.sheybani1993@hotmail.com>
2020-02-03 23:35:46 +0330

    send result to console has been added to console parser

--- Convertor/framework/console_parser.py
+++ Convertor/framework/console_parser.py
@@ -1,12 +1,15 @@
 from Convertor.framework.parser import Parser
 from Convertor.usecase.element_list_factory import ElementListFactory
 from Convertor.usecase.nested_convert import NestedConvert
+from Convertor.framework.send_result import SendResult
 
 class ConsoleParser(Parser):
     def __init__(self, element_list_facotory : ElementListFactory, 
-                    nested_convert : NestedConvert):
+                    nested_convert : NestedConvert,
+                    send_result : SendResult):
         self.__element_list_factory = element_list_facotory
         self.__nested_convert = nested_convert
+        self.__send_result = send_result
     
     def parse(self, input_data : str, args : list):
         if len(input_data) == 0:
@@ -16,4 +19,5 @@ class ConsoleParser(Parser):
             raise ValueError("No Args")
 
         element_list = self.__element_list_factory.create_element_list_by_json_str(input_data)
-        self.__nested_convert(element_list, args)
+        result = self.__nested_convert.convert(element_list, args)
+        self.__send_result.send(result)
--- Convertor/tests/framework/test_console_parser.py
+++ Convertor/tests/framework/test_console_parser.py
@@ -5,13 +5,17 @@ from mock import Mock
 from Convertor.framework.console_parser import ConsoleParser
 from Convertor.usecase.element_list_factory import ElementListFactory
 from Convertor.usecase.nested_convert import NestedConvert
+from Convertor.framework.send_result import SendResult
 from Convertor.entity.element import Element
 
 class TestConsoleParser:
     def setup_method(self, method):
         self.__element_list_facotory = Mock(spec=ElementListFactory)
         self.__nested_convert = Mock(spec=NestedConvert)
-        self.__console_parser = ConsoleParser(self.__element_list_facotory, self.__nested_convert)
+        self.__send_result = Mock(spec=SendResult)
+        self.__console_parser = ConsoleParser(self.__element_list_facotory, 
+                                                self.__nested_convert,
+                                                self.__send_result)
 
     def test_GivenEmptyInptDataWhenCallParseThenItShouldRaiseNoInputDataValueError(self):
         try:
@@ -39,7 +43,17 @@ class TestConsoleParser:
         elemets_list = [Element({"input" : "value"})]
         self.__element_list_facotory.create_element_list_by_json_str.return_value = elemets_list
         self.__console_parser.parse(input_data, args)
-        self.__nested_convert.assert_called_once_with(elemets_list, args)
+        self.__nested_convert.convert.assert_called_once_with(elemets_list, args)
+    
+    def test_GivenInputDataWhenCallParseThenItShouldCallSendResultOnce(self):
+        input_data = '[{"input": "value"}]'
+        args = ["arg1", "arg2"]
+        elemets_list = [Element({"input" : "value"})]
+        result = {"input": [{"value"}]}
+        self.__element_list_facotory.create_element_list_by_json_str.return_value = elemets_list
+        self.__nested_convert.convert.return_value = result
+        self.__console_parser.parse(input_data, args)
+        self.__send_result.send.assert_called_once_with(result)
     
     
 
