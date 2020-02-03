844c9f5c4907fd890838e20231e9c926b9242e5c
hadisheybani <hadi.sheybani1993@hotmail.com>
2020-02-03 23:14:57 +0330

    nested convert has been added to console parser

--- Convertor/framework/console_parser.py
+++ Convertor/framework/console_parser.py
@@ -1,9 +1,12 @@
 from Convertor.framework.parser import Parser
 from Convertor.usecase.element_list_factory import ElementListFactory
+from Convertor.usecase.nested_convert import NestedConvert
 
 class ConsoleParser(Parser):
-    def __init__(self, element_list_facotory : ElementListFactory):
+    def __init__(self, element_list_facotory : ElementListFactory, 
+                    nested_convert : NestedConvert):
         self.__element_list_factory = element_list_facotory
+        self.__nested_convert = nested_convert
     
     def parse(self, input_data : str, args : list):
         if len(input_data) == 0:
@@ -13,3 +16,4 @@ class ConsoleParser(Parser):
             raise ValueError("No Args")
 
         element_list = self.__element_list_factory.create_element_list_by_json_str(input_data)
+        self.__nested_convert(element_list, args)
--- Convertor/tests/framework/test_console_parser.py
+++ Convertor/tests/framework/test_console_parser.py
@@ -4,12 +4,14 @@ from mock import Mock
 
 from Convertor.framework.console_parser import ConsoleParser
 from Convertor.usecase.element_list_factory import ElementListFactory
+from Convertor.usecase.nested_convert import NestedConvert
+from Convertor.entity.element import Element
 
 class TestConsoleParser:
     def setup_method(self, method):
         self.__element_list_facotory = Mock(spec=ElementListFactory)
-        self.__console_parser = ConsoleParser(self.__element_list_facotory)
-        pass
+        self.__nested_convert = Mock(spec=NestedConvert)
+        self.__console_parser = ConsoleParser(self.__element_list_facotory, self.__nested_convert)
 
     def test_GivenEmptyInptDataWhenCallParseThenItShouldRaiseNoInputDataValueError(self):
         try:
@@ -26,10 +28,18 @@ class TestConsoleParser:
             assert_that(e.args[0], equal_to("No Args"))
     
     def test_GivenInputDataWhenCallParseThenItShouldCallElementListFactoryOnce(self):
-        input_data = '[{"input", "value"}]'
+        input_data = '[{"input": "value"}]'
         args = ["arg1", "arg2"]
         self.__console_parser.parse(input_data, args)
         self.__element_list_facotory.create_element_list_by_json_str.assert_called_once_with(input_data)
+    
+    def test_GivenInputDataWhenCallParseThenItShouldCallNestedConvertOnce(self):
+        input_data = '[{"input": "value"}]'
+        args = ["arg1", "arg2"]
+        elemets_list = [Element({"input" : "value"})]
+        self.__element_list_facotory.create_element_list_by_json_str.return_value = elemets_list
+        self.__console_parser.parse(input_data, args)
+        self.__nested_convert.assert_called_once_with(elemets_list, args)
     
     
 
