e0daea3f8a66d254a59f1b0a1e690c36d1605bae
hadisheybani <hadi.sheybani1993@hotmail.com>
2020-02-04 23:15:25 +0330

    run converter has been modified

--- run_converter.py
+++ run_converter.py
@@ -10,13 +10,16 @@ if len(sys.argv) == 1:
     print(help_command)
 else:
     if sys.stdin.isatty():
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'convertor_server.settings')
         if sys.argv[1] == '--runtests':
+            print('Convertor Tests:')
             run_tests()
+            print('Rest API Tests:')
+            execute_from_command_line(['manage.py', 'test'])
         if sys.argv[1] == '--help':
             print(help_command)
-        if sys.argv[1] == 'runserver':
-            os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'convertor_server.settings')
-            execute_from_command_line(sys.argv)
+        if sys.argv[1] == '--runserver':
+            execute_from_command_line(['manage.py', 'runserver'])
     else:
         data = sys.stdin.readlines()
         input_data = str()
