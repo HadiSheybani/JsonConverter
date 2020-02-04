29a967fca7ff6db62043eb332e58b6fc8b7082b6
h.sheybani <h.sheybani@faraz.com>
2020-02-04 12:05:50 +0330

    help command has been modified

--- Convertor/help_command.py
+++ Convertor/help_command.py
@@ -6,12 +6,17 @@ Converter Program Will Convert a json array to nested dictionary of dictionaries
 ******************************************************************
 Usage:
 
-    runtests    will run all of tests
+    --runtests    will run all of tests
         example:
             python run_converter runtests
 
-    help        show usage
+
+    --help        show usage
+
 
     you can run program by console like this:
-        cat input | python run_converter.py nesting_level_1 nesting_level_2 ... nesting_level_n
+        Linux:
+            cat input | python run_converter.py nesting_level_1 nesting_level_2 ... nesting_level_n
+        Windows:
+            type input | python run_convertor.py nesting_level_1 nesting_level_2 ... nesting_level_n
 """
--- run_converter.py
+++ run_converter.py
@@ -8,19 +8,16 @@ if len(sys.argv) == 1:
     print(help_command)
 else:
     if sys.stdin.isatty():
-        if sys.argv[1] == 'runtests':
+        if sys.argv[1] == '--runtests':
             run_tests()
-        if sys.argv[1] == 'help':
+        if sys.argv[1] == '--help':
             print(help_command)
     else:
         data = sys.stdin.readlines()
         input_data = str()
         for d in data:
             input_data = input_data + d
-        argv = ['console']
-        argv.append(input_data)
-        for flag in sys.argv[1:]:
-            argv.append(flag)
+        
         convertor = Convertor()
         result = convertor.convert(input_data, sys.argv[1:])
         print(json.dumps(result, indent=4, sort_keys=True))
